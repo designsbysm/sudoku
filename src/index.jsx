1e7a376e982bc890e0362994e6cb3188a11d03cd
Scott A. Matthews <scott@designsbysm.com>
2019-06-30 10:40:14 -0700

    progress

--- src/index.jsx
+++ src/index.jsx
@@ -15,7 +15,7 @@ const allowCellUpdate = (cells, current) => {
     return false;
   }
 
-  const { predefined } = cells[row][column];
+  const { predefined } = cells[row - 1][column - 1];
   if (predefined) {
     return false;
   }
@@ -23,22 +23,109 @@ const allowCellUpdate = (cells, current) => {
   return true;
 };
 
-const getGridID = (col, row) => {
-  let grid = 0;
+const applyPossibleValues = (cells, possibles) => {
+  const update = [ ...cells ];
 
-  if ((col + 1) / 3 > 2) {
-    grid = 2;
-  } else if ((col + 1) / 3 > 1) {
-    grid = 1;
+  for (let row = 1; row < 10; row++) {
+    for (let column = 1; column < 10; column++) {
+      update[row - 1][column - 1].notes = possibles[row - 1][column - 1].possible;
+    }
   }
 
-  if ((row + 1) / 3 > 2) {
+  return update;
+};
+
+const initialCurrentCell = {
+  column: -1,
+  grid: -1,
+  row: -1,
+};
+
+const getGridID = (col, row) => {
+  let grid = 1;
+
+  if (col / 3 > 2) {
+    grid = 3;
+  } else if (col / 3 > 1) {
+    grid = 2;
+  }
+
+  if (row / 3 > 2) {
     grid += 6;
-  } else if ((row + 1) / 3 > 1) {
+  } else if (row / 3 > 1) {
     grid += 3;
   }
 
   return grid;
+};
+
+const getPossibleValues = cells => {
+  const hashPossibles = grid => grid.map(r => r.map(cell => cell.possible.join(""))
+    .join(""))
+    .join("");
+
+  const hasChanges = (before, after) => hashPossibles(before) !== hashPossibles(after);
+
+  let possibles = cells.map(r => {
+    return r.map(cell => {
+      const { column, grid, row, value } = cell;
+
+      return {
+        column,
+        grid,
+        possible: value ? [ value ] : [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9, 
+        ],
+        row,
+      };
+    });
+  });
+
+  let continueSearch = true;
+
+  /* eslint-disable sort-keys */
+  const pipeline = {
+    crme: possibleCRME,
+    loneRangers: possibleLoneRangers,
+    twins: possibleTwins,
+    triplets: possibleTriplets,
+  };
+  /* eslint-enable sort-keys */
+
+  const scores = {};
+
+  while (continueSearch) {
+    let changes = false;
+
+    for (const key of Object.keys(pipeline)) {
+      const fn = pipeline[key];
+
+      const result = fn(possibles);
+      changes = hasChanges(possibles, result);
+
+      if (changes) {
+        scores[key] = scores[key] ? scores[key] + 1 : 1;
+        possibles = result;
+        break;
+      }
+    }
+
+    if (!changes) {
+      continueSearch = false;
+    }
+  }
+
+  console.log(scores);
+
+  return possibles;
 };
 
 const hintCellValue = (cells, current) => {
@@ -48,9 +135,12 @@ const hintCellValue = (cells, current) => {
   }
 
   const update = [ ...cells ];
-  update[row][column].predefined = true;
-  update[row][column].status = "";
-  update[row][column].value = update[row][column].solution;
+
+  if (update[row - 1][column - 1].solution) {
+    update[row - 1][column - 1].predefined = true;
+    update[row - 1][column - 1].status = "";
+    update[row - 1][column - 1].value = update[row - 1][column - 1].solution;
+  }
 
   return update;
 };
@@ -143,25 +233,25 @@ const moveCurrent = (key, col, row) => {
 
   switch (key) {
     case "ArrowLeft":
-      if (col > 0) {
+      if (col > 1) {
         newColumn -= 1;
       }
       break;
 
     case "ArrowRight":
-      if (col < 8) {
+      if (col < 9) {
         newColumn += 1;
       }
       break;
 
     case "ArrowUp":
-      if (row > 0) {
+      if (row > 1) {
         newRow -= 1;
       }
       break;
 
     case "ArrowDown":
-      if (row < 8) {
+      if (row < 9) {
         newRow += 1;
       }
       break;
@@ -182,52 +272,241 @@ const newGame = cells => {
   const update = [ ...cells ];
 
   /* eslint-disable array-element-newline */
+  // const solution = [
+  //   [ 3, 9, 5, 4, 6, 7, 1, 8, 2 ],
+  //   [ 1, 4, 6, 2, 8, 9, 5, 3, 7 ],
+  //   [ 2, 7, 8, 5, 1, 3, 6, 9, 4 ],
+  //   [ 4, 5, 1, 9, 3, 8, 7, 2, 6 ],
+  //   [ 9, 8, 7, 6, 4, 2, 3, 1, 5 ],
+  //   [ 6, 2, 3, 7, 5, 1, 9, 4, 8 ],
+  //   [ 5, 6, 2, 3, 9, 4, 8, 7, 1 ],
+  //   [ 8, 3, 4, 1, 7, 5, 2, 6, 9 ],
+  //   [ 7, 1, 9, 8, 2, 6, 4, 5, 3 ],
+  // ];
+
+  // const puzzle = [
+  //   [ 0, 0, 5, 4, 0, 0, 1, 8, 0 ],
+  //   [ 1, 4, 6, 0, 8, 0, 5, 0, 0 ],
+  //   [ 0, 7, 0, 0, 1, 3, 0, 0, 0 ],
+  //   [ 4, 5, 1, 0, 0, 8, 7, 0, 6 ],
+  //   [ 0, 8, 0, 0, 0, 0, 0, 1, 0 ],
+  //   [ 6, 0, 3, 7, 0, 0, 9, 4, 8 ],
+  //   [ 0, 0, 0, 3, 9, 0, 0, 7, 0 ],
+  //   [ 0, 0, 4, 0, 7, 0, 2, 6, 9 ],
+  //   [ 0, 1, 9, 0, 0, 6, 4, 0, 0 ],
+  // ];
+
   const solution = [
-    [ 3, 9, 5, 4, 6, 7, 1, 8, 2 ],
-    [ 1, 4, 6, 2, 8, 9, 5, 3, 7 ],
-    [ 2, 7, 8, 5, 1, 3, 6, 9, 4 ],
-    [ 4, 5, 1, 9, 3, 8, 7, 2, 6 ],
-    [ 9, 8, 7, 6, 4, 2, 3, 1, 5 ],
-    [ 6, 2, 3, 7, 5, 1, 9, 4, 8 ],
-    [ 5, 6, 2, 3, 9, 4, 8, 7, 1 ],
-    [ 8, 3, 4, 1, 7, 5, 2, 6, 9 ],
-    [ 7, 1, 9, 8, 2, 6, 4, 5, 3 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
   ];
 
   const puzzle = [
-    [ 0, 0, 5, 4, 0, 0, 1, 8, 0 ],
-    [ 1, 4, 6, 0, 8, 0, 5, 0, 0 ],
-    [ 0, 7, 0, 0, 1, 3, 0, 0, 0 ],
-    [ 4, 5, 1, 0, 0, 8, 7, 0, 6 ],
-    [ 0, 8, 0, 0, 0, 0, 0, 1, 0 ],
-    [ 6, 0, 3, 7, 0, 0, 9, 4, 8 ],
-    [ 0, 0, 0, 3, 9, 0, 0, 7, 0 ],
-    [ 0, 0, 4, 0, 7, 0, 2, 6, 9 ],
-    [ 0, 1, 9, 0, 0, 6, 4, 0, 0 ],
+    [ 0, 0, 0, 0, 3, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 6, 0, 7, 0, 0, 9 ],
+    [ 0, 0, 0, 9, 0, 0, 7, 3, 0 ],
+    [ 0, 0, 0, 0, 0, 6, 0, 0, 4 ],
+    [ 1, 8, 0, 4, 0, 5, 0, 2, 7 ],
+    [ 7, 0, 0, 3, 0, 0, 0, 0, 0 ],
+    [ 0, 7, 1, 0, 0, 9, 0, 0, 0 ],
+    [ 2, 0, 0, 1, 0, 8, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 7, 0, 0, 0, 0 ],
   ];
   /* eslint-enable array-element-newline */
 
-  for (let row = 0; row < 9; row++) {
-    for (let column = 0; column < 9; column++) {
-      if (puzzle[row][column]) {
-        update[row][column].predefined = true;
-        update[row][column].value = puzzle[row][column];
+  for (let row = 1; row < 10; row++) {
+    for (let column = 1; column < 10; column++) {
+      if (puzzle[row - 1][column - 1]) {
+        update[row - 1][column - 1].predefined = true;
+        update[row - 1][column - 1].value = puzzle[row - 1][column - 1];
       }
 
-      update[row][column].solution = solution[row][column];
+      update[row - 1][column - 1].solution = solution[row - 1][column - 1];
     }
   }
 
   return update;
 };
 
+const possibleCreateUpdate = (cells, possibles) => {
+  const updates = cells.map(r => {
+    return r.map(cell => {
+      return { ...cell };
+    });
+  });
+
+  possibles.flat()
+    .forEach(cell => {
+      const { column, possible, row } = cell;
+
+      updates[row - 1][column - 1].possible = possible;
+    });
+
+  return updates;
+};
+
+const possibleCRME = cells => {
+  const searchSet = set => {
+    const usedValues = [];
+
+    set.forEach(cell => {
+      if (cell.possible.length === 1) {
+        usedValues.push(...cell.possible);
+      }
+    });
+
+    usedValues.forEach(used => {
+      set.forEach(cell => {
+        if (cell.possible.length > 1) {
+          cell.possible = cell.possible.filter(value => value !== used);
+        }
+      });
+    });
+  };
+
+  const { columns, grids, rows } = splitCRM(cells);
+
+  [
+    ...rows,
+    ...columns,
+    ...grids, 
+  ].forEach(set => {
+    searchSet(set);
+  });
+
+  return possibleCreateUpdate(cells, rows);
+};
+
+const possibleLoneRangers = cells => {
+  const searchSet = set => {
+    const count = {};
+
+    set.forEach(cell => {
+      if (cell.possible.length > 1) {
+        cell.possible.forEach(value => {
+          count[value] = count[value] ? count[value] + 1 : 1;
+        });
+      }
+    });
+
+    for (const key of Object.keys(count)) {
+      if (count[key] === 1) {
+        set.forEach(cell => {
+          if (cell.possible.includes(parseInt(key, 10))) {
+            cell.possible = cell.possible.filter(value => value === parseInt(key, 10));
+          }
+        });
+      }
+    }
+  };
+
+  const { columns, grids, rows } = splitCRM(cells);
+
+  [
+    ...rows,
+    ...columns,
+    ...grids, 
+  ].forEach(set => {
+    searchSet(set);
+  });
+
+  return possibleCreateUpdate(cells, rows);
+};
+
+const possibleTriplets = cells => {
+  const searchSet = set => {
+    const update = [ ...set ];
+    const possibles = set.map(cell => cell.possible.join(""));
+    const count = {};
+
+    possibles.forEach(cell => {
+      if (cell.length === 2) {
+        count[cell] = count[cell] ? count[cell] + 1 : 1;
+      }
+    });
+
+    for (const key of Object.keys(count)) {
+      if (count[key] === 2) {
+        update.forEach(cell => {
+          if (cell.possible.length > 2) {
+            const values = [ ...key ].map(value => parseInt(value, 10));
+
+            cell.possible = cell.possible.filter(value => !values.includes(value));
+          }
+        });
+      }
+    }
+
+    return update;
+  };
+
+  const { columns, grids, rows } = splitCRM(cells);
+
+  [
+    ...rows,
+    ...columns,
+    ...grids, 
+  ].forEach(set => {
+    searchSet(set);
+  });
+
+  return possibleCreateUpdate(cells, rows);
+};
+
+const possibleTwins = cells => {
+  const searchSet = set => {
+    const update = [ ...set ];
+    const possibles = set.map(cell => cell.possible.join(""));
+    const count = {};
+
+    possibles.forEach(cell => {
+      if (cell.length === 2) {
+        count[cell] = count[cell] ? count[cell] + 1 : 1;
+      }
+    });
+
+    for (const key of Object.keys(count)) {
+      if (count[key] === 2) {
+        update.forEach(cell => {
+          if (cell.possible.length > 2) {
+            const values = [ ...key ].map(value => parseInt(value, 10));
+
+            cell.possible = cell.possible.filter(value => !values.includes(value));
+          }
+        });
+      }
+    }
+
+    return update;
+  };
+
+  const { columns, grids, rows } = splitCRM(cells);
+
+  [
+    ...rows,
+    ...columns,
+    ...grids, 
+  ].forEach(set => {
+    searchSet(set);
+  });
+
+  return possibleCreateUpdate(cells, rows);
+};
+
 const setupCells = () => {
   const cells = [];
 
-  for (let row = 0; row < 9; row++) {
+  for (let row = 1; row < 10; row++) {
     const rowArray = [];
 
-    for (let column = 0; column < 9; column++) {
+    for (let column = 1; column < 10; column++) {
       const grid = getGridID(column, row);
 
       rowArray.push({
@@ -247,6 +526,32 @@ const setupCells = () => {
   return cells;
 };
 
+const splitCRM = cells => {
+  /* eslint-disable array-element-newline */
+  const rows = [ [], [], [], [], [], [], [], [], [] ];
+
+  const columns = [ [], [], [], [], [], [], [], [], [] ];
+
+  const grids = [ [], [], [], [], [], [], [], [], [] ];
+  /* eslint-enable array-element-newline */
+
+  cells.flat()
+    .forEach(cell => {
+      const { column, grid, row } = cell;
+      const newCell = { ...cell };
+
+      columns[column - 1].push(newCell);
+      grids[grid - 1].push(newCell);
+      rows[row - 1].push(newCell);
+    });
+
+  return {
+    columns,
+    grids,
+    rows,
+  };
+};
+
 const updateCellNotes = (key, cells, current) => {
   if (!allowCellUpdate(cells, current) || key === "backspace") {
     return null;
@@ -256,7 +561,7 @@ const updateCellNotes = (key, cells, current) => {
   const newValue = parseInt(key, 10);
 
   const update = [ ...cells ];
-  let notes = update[row][column].notes;
+  let notes = update[row - 1][column - 1].notes;
 
   if (notes.includes(newValue)) {
     notes = notes.filter(item => item !== newValue);
@@ -265,7 +570,7 @@ const updateCellNotes = (key, cells, current) => {
     notes.push(newValue);
   }
 
-  update[row][column].notes = notes;
+  update[row - 1][column - 1].notes = notes;
 
   return update;
 };
@@ -283,43 +588,14 @@ const updateCellValue = (key, cells, current) => {
   }
 
   const update = [ ...cells ];
-  update[row][column].notes = [];
-  update[row][column].status = isMoveValid(cells, current, newValue) ? "" : "error";
-  update[row][column].value = newValue;
+  update[row - 1][column - 1].notes = [];
+  update[row - 1][column - 1].status = isMoveValid(cells, current, newValue) ? "" : "error";
+  update[row - 1][column - 1].value = newValue;
 
   return update;
 };
 
 const App = () => {
-  const emptyCurrent = {
-    column: -1,
-    grid: -1,
-    row: -1,
-  };
-
-  const [
-    cells,
-    setCells, 
-  ] = useState(setupCells());
-
-  const [
-    current,
-    setCurrent, 
-  ] = useState(emptyCurrent);
-
-  const [
-    penMode,
-    setPenMode, 
-  ] = useState(true);
-
-  useEffect(() => {
-    window.addEventListener("keydown", handleKeydown);
-
-    return () => {
-      window.removeEventListener("keydown", handleKeydown);
-    };
-  });
-
   const handleKeydown = event => {
     const { key } = event;
 
@@ -372,7 +648,7 @@ const App = () => {
   };
 
   const resetCurrent = () => {
-    setCurrent(emptyCurrent);
+    setCurrent(initialCurrentCell);
   };
 
   const updateCell = key => {
@@ -393,7 +669,44 @@ const App = () => {
     setCells(update);
   };
 
-  newGame(cells);
+  const [
+    cells,
+    setCells, 
+  ] = useState([]);
+
+  const [
+    current,
+    setCurrent, 
+  ] = useState(initialCurrentCell);
+
+  const [
+    mounted,
+    setMounted, 
+  ] = useState(false);
+
+  const [
+    penMode,
+    setPenMode, 
+  ] = useState(true);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeydown);
+    };
+  });
+
+  useEffect(() => {
+    if (!mounted) {
+      const game = newGame(setupCells());
+      const possibles = getPossibleValues(game);
+      const updated = applyPossibleValues(game, possibles);
+
+      setCells(updated);
+      setMounted(true);
+    }
+  }, [ mounted ]);
 
   return (
     <section
